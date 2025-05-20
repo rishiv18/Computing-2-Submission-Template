7f7c82f6fb39acf4f6cd0b556207cc0a31e9e732
rishiv18 <rishi.viswanath23@imperial.ac.uk>
2025-05-20 11:31:37 +0100

    dwad
    wdadw

---
+++ web-app/bento_block.js
@@ -0,0 +1,8 @@
+import R from "./ramda.js";
+/**
+ * bento_block.js is a module to model and play "Blockus" and related games.
+ * https://en.wikipedia.org/wiki/Blokus
+ * @namespace bento_block
+ * @author A. Rishi Viswanath
+ * @version 05/2025
+ */
